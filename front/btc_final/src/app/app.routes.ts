aa386b663bf2f0a2052a45274bef5d83a0aa432a
Tyrsonnur <andresf199810@hotmail.com>
2024-06-27 21:28:37 -0500

    Actualizacion navbar, encuentranos 27/06/2024

--- front/btc_final/src/app/app.routes.ts
+++ front/btc_final/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { EncuentranosComponent } from './components/encuentranos/encuentranos.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
+  { path: "Inicio", component: InicioComponent },
+  { path: "encuentranos", component: EncuentranosComponent }
   //{ path:}
 ];
 
