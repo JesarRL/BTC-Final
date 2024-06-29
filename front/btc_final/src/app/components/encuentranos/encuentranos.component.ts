30ff138e569e3fdbb13d17953d39a5ad59392d56
Tyrsonnur <andresf199810@hotmail.com>
2024-06-28 21:53:53 -0500

    actualizacion 28/06/2024

--- front/btc_final/src/app/components/encuentranos/encuentranos.component.ts
+++ front/btc_final/src/app/components/encuentranos/encuentranos.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-encuentranos',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './encuentranos.component.html',
   styleUrl: './encuentranos.component.css'
 })
--- front/btc_final/src/app/components/inicio/inicio.component.ts
+++ front/btc_final/src/app/components/inicio/inicio.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+
+  ],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
