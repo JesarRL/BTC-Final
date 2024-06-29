0ecd0c668718d183249edd69b5eb19b952b6dff8
Tyrsonnur <andresf199810@hotmail.com>
2024-06-28 21:53:30 -0500

    Actualizacion conexion paginas

--- front/btc_final/src/app/app.routes.ts
+++ front/btc_final/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { EncuentranosComponent } from './components/encuentranos/encuentranos.component';
+import { PreguntasComponent } from './components/preguntas/preguntas.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
   { path: "Inicio", component: InicioComponent },
-  { path: "encuentranos", component: EncuentranosComponent }
+  { path: "Encuentranos", component: EncuentranosComponent },
+  { path: "Preguntas", component: PreguntasComponent }
   //{ path:}
 ];
 
