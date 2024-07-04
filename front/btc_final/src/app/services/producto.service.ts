d7978a9cc60ca8b5adfa7d8b17f2b455d11b5614
Tyrsonnur <andresf199810@hotmail.com>
2024-07-03 21:46:53 -0500

    Actualizacion logica 03/07/2024

--- front/btc_final/src/app/services/producto.service.ts
+++ front/btc_final/src/app/services/producto.service.ts
@@ -2,21 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ProductoService {
 
-  UrlApi: string = "http://localhost:3000/best-trucks-colombia"
+    UrlApi: string = "http://localhost:3000/best-trucks-colombia"
 
-  constructor(private http:HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  getCamionetas(tipo:any = ""){
-    if(tipo == "G"){
-      return this.http.get(`${this.UrlApi}/vehiculos/listar-gasolina`)
-    }else if(tipo == "D"){
-      return this.http.get(`${this.UrlApi}/vehiculos/listar-diesel`)
-    }else{
-      return this.http.get(`${this.UrlApi}/vehiculos/listar-inventario`)
+    getCamionetas(tipo: any = "") {
+        if (tipo == "G") {
+            return this.http.get(`${this.UrlApi}/vehiculos/listar-gasolina`)
+        } else if (tipo == "D") {
+            return this.http.get(`${this.UrlApi}/vehiculos/listar-diesel`)
+        } else {
+            return this.http.get(`${this.UrlApi}/vehiculos/listar-inventario`)
+        }
     }
-  }
+
+
+    getCamioneta(id:any){
+        return this.http.get(`${this.UrlApi}/vehiculos/buscar_vehiculo/${id}`)
+    }
 }
