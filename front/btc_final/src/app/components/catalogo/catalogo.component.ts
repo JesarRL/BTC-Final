91c7b5556d961ca2b3768d250ac9507a15daf61f
JesarRL <ljesar01@gmail.com>
2024-07-03 21:40:07 -0500

    subiendo avances

--- front/btc_final/src/app/components/catalogo/catalogo.component.ts
+++ front/btc_final/src/app/components/catalogo/catalogo.component.ts
@@ -11,14 +11,11 @@ import { ProductoService } from '../../services/producto.service';
 })
 export class CatalogoComponent {
 
-    dataVehiculos_slider1_gasolina: any = []
-    dataVehiculos_slider2_gasolina: any = []
-    dataVehiculos_slider3_gasolina: any = []
+
+    dataVehiculos_gasolina: any
 
 
-    dataVehiculos_slider1_diesel: any = []
-    dataVehiculos_slider2_diesel: any = []
-    dataVehiculos_slider3_diesel: any = []
+    dataVehiculos_diesel: any
 
     constructor(private _apiService: ProductoService, private router:Router) { }
 
@@ -30,13 +27,16 @@ export class CatalogoComponent {
     obtenerVehiculos(tipoConsulta: any = "") {
         this._apiService.getCamionetas(tipoConsulta).subscribe((data: any) => {
             if (tipoConsulta == "G") {
-                this.dataVehiculos_slider1_gasolina.push(data.camionetaData[0], data.camionetaData[1], data.camionetaData[2], data.camionetaData[3])
-                this.dataVehiculos_slider2_gasolina.push(data.camionetaData[4], data.camionetaData[5], data.camionetaData[6], data.camionetaData[7])
-                this.dataVehiculos_slider3_gasolina.push(data.camionetaData[8], data.camionetaData[9])
+                this.dataVehiculos_gasolina = data.camionetaData
+
+                console.log("gasolina: ", this.dataVehiculos_gasolina);
+
+
             } else {
-                this.dataVehiculos_slider1_diesel.push(data.camionetaData[0], data.camionetaData[1], data.camionetaData[2], data.camionetaData[3])
-                this.dataVehiculos_slider2_diesel.push(data.camionetaData[4], data.camionetaData[5], data.camionetaData[6], data.camionetaData[7])
-                this.dataVehiculos_slider3_diesel.push(data.camionetaData[8], data.camionetaData[9])
+                this.dataVehiculos_diesel = data.camionetaData
+
+                console.log("diesel: ", this.dataVehiculos_diesel);
+
             }
         })
     }
