532bae495ffcbe46c9e67e84db747e7448d4ed68
JesarRL <ljesar01@gmail.com>
2024-07-16 19:52:08 -0500

    subiendo avance

--- front/btc_final/src/app/app.component.ts
+++ front/btc_final/src/app/app.component.ts
@@ -4,15 +4,15 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,
-    NavbarComponent,
-    FooterComponent
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet,
+        NavbarComponent,
+        FooterComponent
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'btc_final';
+    title = 'btc_final';
 }
