12caec858733b4a5f8fe756a25051298f611e718
JesarRL <ljesar01@gmail.com>
2024-07-23 19:54:50 -0500

    subiendo avance front

--- front/btc_final/src/app/components/catalogocompleto/catalogocompleto.component.ts
+++ front/btc_final/src/app/components/catalogocompleto/catalogocompleto.component.ts
@@ -3,24 +3,24 @@ import { ProductoService } from '../../services/producto.service';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-catalogocompleto',
-  standalone: true,
-  imports: [RouterLink],
-  templateUrl: './catalogocompleto.component.html',
-  styleUrl: './catalogocompleto.component.css'
+    selector: 'app-catalogocompleto',
+    standalone: true,
+    imports: [RouterLink],
+    templateUrl: './catalogocompleto.component.html',
+    styleUrl: './catalogocompleto.component.css'
 })
 export class CatalogocompletoComponent {
 
     catalogo: any
 
-    constructor(private _apiService: ProductoService, private router:Router){}
+    constructor(private _apiService: ProductoService, private router: Router) { }
 
-    ngOnInit(): void{
+    ngOnInit(): void {
         this.obtenerVehiculos()
     }
 
 
-    obtenerVehiculos(){
+    obtenerVehiculos() {
         this._apiService.getCamionetas().subscribe((data: any) => {
 
             this.catalogo = data.camionetaData
